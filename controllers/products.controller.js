d8114439cff4e6ff12004728b1aeea6dc4003c2f
Mohit <mohitk.albiorix2gmail.com>
2023-02-17 18:44:23 +0530

    Worked on add extra functionality in product app.

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,25 +1,28 @@
 const Product = require('../models/products.model');
 
 exports.getAddProducts = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product'
     });
 }
 
 exports.postAddProducts = (req, res, next) => {
-    const product = new Product(req.body.title)
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, price, description);
     product.save();
     res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('admin/products', {
             prods: products,
-            pageTitle: 'Shop',
-            path: '/',
-            hasProducts: products.length > 0
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
         });
     })
 }
--- expapp.js
+++ expapp.js
@@ -4,15 +4,15 @@ const bodyparser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
-const errorController = require('./controllers/404Error.controllers');
+const errorController = require('./controllers/404Error.controller');
 
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use('/admin',adminRoutes.routes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
-app.use(express.static(path.join(__dirname,'public'))); // use for static servng files
+app.use(express.static(path.join(__dirname, 'public'))); // use for static servng files
 
 app.use(errorController.getErrors);
-app.set('view engine','ejs');
-app.set('views', 'views') 
+app.set('view engine', 'ejs');
+app.set('views', 'views')
 
 app.listen(3000);
